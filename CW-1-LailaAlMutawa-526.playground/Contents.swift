c922c439c10bed3100d0c9b653d7d653942a2605
ljmutawa <laila.almutawa2004@gmail.com>
2020-06-13 19:10:13 +0300

    done

---
+++ CW-1-LailaAlMutawa-526.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var str = "Hello, playground"
+let name = "Laila Al Mutawa"
+var height = "162 cm tall"
+var age = "15 years old"
+var hobby1 = "read"
+var hobby2 = "play Badminton"
+
+print ("Hello! My name is \(name), I like to \(hobby1) and to \(hobby2). I am \(age), and \(height)")
+
