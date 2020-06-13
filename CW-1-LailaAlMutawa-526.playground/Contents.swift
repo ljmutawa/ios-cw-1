4442031735608ba5697308b2db31762ffc233076
ljmutawa <laila.almutawa2004@gmail.com>
2020-06-13 19:59:50 +0300

    done

--- CW-1-LailaAlMutawa-526.playground/Contents.swift
+++ CW-1-LailaAlMutawa-526.playground/Contents.swift
@@ -1,11 +1,34 @@
-import UIKit
-
-var str = "Hello, playground"
 let name = "Laila Al Mutawa"
-var height = "162 cm tall"
-var age = "15 years old"
+var height = 1.62
+var age = 15
 var hobby1 = "read"
 var hobby2 = "play Badminton"
 
 print ("Hello! My name is \(name), I like to \(hobby1) and to \(hobby2). I am \(age), and \(height)")
+
+let friendName = "Fatima Al Ghanim"
+var friendHeight = 1.67
+var friendAge = 15
+var friendHobby1 = "swim"
+var friendHobby2 = "play soccer"
+
+name == friendName
+height == friendHeight
+age == friendAge
+hobby1 == friendHobby1
+hobby2 == friendHobby2
+
+height > friendHeight
+height < friendHeight
+height >= friendHeight
+height <= friendHeight
+height == friendHeight
+height != friendHeight
+
+age > friendAge
+age < friendAge
+age >= friendAge
+age <= friendAge
+age == friendAge
+age != friendAge
 
